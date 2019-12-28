dc570e643d2a9cb82a8c3dab86083e184f432141
Dima Gerasimov <karlicoss@gmail.com>
2019-12-28 14:02:13 +0000

    add login

--- export.py
+++ export.py
@@ -19,16 +19,36 @@ def get_json(**params) -> Json:
     return workouts
 
 
+def login(email: str):
+    from kython import import_from
+    endoapi = import_from('/L/repos/endoapi', 'endoapi')
+    import endoapi.endomondo # type: ignore
+
+    print(f"Logging in as {email}")
+    password = input('Your password: ')
+    endomondo = endoapi.endomondo.Endomondo(email=email, password=password)
+    print('Your token:')
+    print(endomondo.token)
+
+
 def main():
     # TODO add logger configuration to export_helper?
     # TODO autodetect logzero?
     from export_helper import setup_parser
     parser = argparse.ArgumentParser("Tool to export your personal Endomondo data")
     setup_parser(parser=parser, params=['email', 'token']) # TODO exports -- need help for each param?
+    parser.add_argument('--login', action='store_true', help='''
+This will log you in and give you the token (you'll need your password).
+You only need to do it once, after that just store the token and use it.
+    ''')
     args = parser.parse_args()
 
     params = args.params
     dumper = args.dumper
+
+    if args.login:
+        login(email=params['email'])
+        return
 
     j = get_json(**params)
     js = json.dumps(j, indent=1, ensure_ascii=False)
