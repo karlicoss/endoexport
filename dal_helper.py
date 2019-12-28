c2ab6982cced4f6cc4093d81f7739d2bd95bf1a7
Dima Gerasimov <karlicoss@gmail.com>
2019-12-28 16:57:28 +0000

    add data access layer and demo

---
+++ dal_helper.py
@@ -0,0 +1,40 @@
+import argparse
+from glob import glob
+from pathlib import Path
+from typing import Any, Dict, Union
+
+import IPython # type: ignore
+
+
+PathIsh = Union[str, Path]
+Json = Dict[str, Any] # TODO Mapping?
+
+
+def main(*, DAL, demo=None):
+    p = argparse.ArgumentParser()
+    p.add_argument('--source', type=str, required=True)
+    p.add_argument('--no-glob', action='store_true')
+    p.add_argument('-i', '--interactive', action='store_true', help='Start Ipython session to play with data')
+    args = p.parse_args()
+    if '*' in args.source and not args.no_glob:
+        sources = glob(args.source)
+    else:
+        sources = [args.source]
+
+    # logger.debug('using %s', sources)
+    dao = DAL(list(sorted(sources)))
+    print(dao)
+    # TODO autoreload would be nice... https://github.com/ipython/ipython/issues/1144
+    # TODO maybe just launch through ipython in the first place?
+    if args.interactive:
+        IPython.embed(header="Feel free to mess with 'dao' object in the interactive shell")
+    else:
+        assert demo is not None
+        demo(dao=dao)
+
+
+def logger(logger, **kwargs):
+    from kython.klogging import LazyLogger
+    # TODO FIXME vendorize
+    return LazyLogger(logger, **kwargs)
+
