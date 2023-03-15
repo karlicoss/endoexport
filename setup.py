98c8805cbcc00187822737ef32c2e0434c4f450e
karlicoss <karlicoss@gmail.com>
2023-03-15 02:19:15 +0000

    CI: bring configs up to date

--- setup.py
+++ setup.py
@@ -1,29 +1,66 @@
+# see https://github.com/karlicoss/pymplate for up-to-date reference
+
+
 from setuptools import setup, find_namespace_packages # type: ignore
 
 
-def main():
+COMMON_DEPS = [
+    # I'm using some upstream unmerged changes, so unfortunately need my own fork
+    'endoapi @ git+https://github.com/karlicoss/endoapi.git',
+    # TODO hmm maybe I need to rename the setup.py for my fork? to make it less confusing
+    # uncomment to test against the filesystem package
+    # 'endoapi @ git+file://DUMMY/path/to/endoapi',
+]
+
+
+EXPORT_DEPS = COMMON_DEPS
+
+DAL_DEPS = COMMON_DEPS
+
+ALL_DEPS = sorted({*EXPORT_DEPS, *DAL_DEPS})
+
+
+def main() -> None:
+    # works with both ordinary and namespace packages
     pkgs = find_namespace_packages('src')
-    pkg = min(pkgs)
-    return setup(
+    pkg = min(pkgs) # lexicographically smallest is the correct one usually?
+    setup(
         name=pkg,
+        use_scm_version={
+            'version_scheme': 'python-simplified-semver',
+            'local_scheme': 'dirty-tag',
+        },
+        setup_requires=['setuptools_scm'],
+
+        # otherwise mypy won't work
+        # https://mypy.readthedocs.io/en/stable/installed_packages.html#making-pep-561-compatible-packages
         zip_safe=False,
+
         packages=pkgs,
         package_dir={'': 'src'},
+        # necessary so that package works with mypy
         package_data={pkg: ['py.typed']},
 
-        install_requires=[
-            # I'm using some upstream unmerged changes, so unfortunately need my own fork
-            'endoapi @ git+https://github.com/karlicoss/endoapi.git',
-            # TODO hmm maybe I need to rename the setup.py for my fork? to make it less confusing
-            # uncomment to test against the filesystem package
-            # 'endoapi @ git+file://DUMMY/path/to/endoapi',
-        ],
+        ## ^^^ this should be mostly automatic and not requiring any changes
+
+        install_requires=ALL_DEPS,
         extras_require={
-            'testing': ['pytest', 'numpy'],
-            'linting': ['pytest', 'mypy'],
+            'dal': DAL_DEPS,
+            'export': EXPORT_DEPS,
+            'testing': [
+                'pytest',
+                'numpy',  # for data generator
+            ],
+            'linting': [
+                'pytest',
+                'mypy',
+                'lxml',  # lxml for mypy coverage report
+                'orjson',  # optional packages
+            ],
         },
     )
 
 
 if __name__ == '__main__':
     main()
+
